3b75edb6eef9c7714878ba2e3db97fddcd8f52e3
jjj <qkrwpdlr@gmail.com>
2019-04-24 15:36:42 +0900

    Updates

---
+++ precache-manifest.0d0a937fdeb9ca6f5c9ddfad077ac812.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6392db6bcb8c275bff88",
+    "url": "/movie_app/static/css/main.89cdca18.chunk.css"
+  },
+  {
+    "revision": "6392db6bcb8c275bff88",
+    "url": "/movie_app/static/js/main.4a108276.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "8995ed65ce443e708012",
+    "url": "/movie_app/static/js/2.f6b90c5d.chunk.js"
+  },
+  {
+    "revision": "6ff61586bb639e4e93817349aa9b7f15",
+    "url": "/movie_app/index.html"
+  }
+];
